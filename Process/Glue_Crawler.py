54dd901287c7458182b794300274c3bbd3edafc3
pranjaliborse2 <111794240+pranjaliborse2@users.noreply.github.com>
2025-06-28 14:48:01 -0500

    Delete Process/Glue_Crawler.py

--- Process/Glue_Crawler.py
+++
@@ -1,8 +0,0 @@
-# Define the function to start Glue Crawler
-
-import boto3
-
-def start_glue_crawler(crawler_name):
-    client = boto3.client('glue', region_name='us-east-1')
-    response = client.start_crawler(Name=crawler_name)
-    return response
